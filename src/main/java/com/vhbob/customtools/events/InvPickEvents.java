6e43d4365f33cdd08d2f45e4af9a9abada06c7b8
Vince <vhorvath2010@gmail.com>
2021-03-14 16:15:27 -0700

    Fixed error with inv pick

--- src/main/java/com/vhbob/customtools/events/InvPickEvents.java
+++ src/main/java/com/vhbob/customtools/events/InvPickEvents.java
@@ -1,6 +1,8 @@
 package com.vhbob.customtools.events;
 
 import com.vhbob.customtools.util.ToolUtils;
+import it.unimi.dsi.fastutil.ints.Int2ObjectAVLTreeMap;
+import org.bouncycastle.util.Arrays;
 import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -8,7 +10,9 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.inventory.ItemStack;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashMap;
 
 public class InvPickEvents implements Listener {
 
@@ -16,9 +20,17 @@ public class InvPickEvents implements Listener {
     public void onBreak(BlockBreakEvent e) {
         if (!e.isCancelled() && ToolUtils.checkPick(e.getPlayer().getInventory().getItemInMainHand(), "preset-tools.inventory.lore")) {
             Collection<ItemStack> drops = e.getBlock().getDrops(e.getPlayer().getInventory().getItemInMainHand());
-            for (ItemStack drop : drops)
-                e.getPlayer().getInventory().addItem(drop);
+            ArrayList<ItemStack> notAdded = new ArrayList<ItemStack>();
+            for (ItemStack item : drops) {
+                HashMap<Integer, ItemStack> items = e.getPlayer().getInventory().addItem(item);
+                if (items.size() > 0) {
+                    notAdded.addAll(items.values());
+                }
+            }
             e.getBlock().setType(Material.AIR);
+            for (ItemStack itemStack : notAdded) {
+                e.getBlock().getWorld().dropItemNaturally(e.getBlock().getLocation(), itemStack);
+            }
         }
     }
 
